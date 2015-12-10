a60d0d5404ba180d0d9d47756b35c95962001398
thien.lm <thienleminh1982@gmail.com>
2015-12-10 20:40:26 +0700

    commit milestone 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,16 @@ class SessionsController < ApplicationController
         @user = User.find_by_email(params[:user][:email])
 
         if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
         	redirect_to users_path
         else
         	render :new
         end
-    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to welcome_index_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'login' => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
